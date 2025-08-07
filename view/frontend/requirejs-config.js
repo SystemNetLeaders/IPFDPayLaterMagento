20878ddd9d8a11aac4925ae230af3828d5c7b17d
mindgoner <contact@bartoszbieniek.pl>
2025-08-07 10:54:13 +0200

    PLM-25

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,14 +1,13 @@
 var config = {
+    deps: [
+        'Creditea_Magento2/js/creditea-venobox-promo'
+    ],
     paths: {
         'creditea-venobox': 'Creditea_Magento2/js/venobox',
-        'creditea-venobox-promo': 'Creditea_Magento2/js/creditea-venobox-promo',
+        'creditea-venobox-promo': 'Creditea_Magento2/js/creditea-venobox-promo'
     },
     shim: {
-        'creditea-venobox': {
-            'deps': ['jquery']
-        },
-        'creditea-venobox-promo': {
-            'deps': ['jquery']
-        }
+        'creditea-venobox': { deps: ['jquery'] },
+        'creditea-venobox-promo': { deps: ['jquery'] }
     }
 };
